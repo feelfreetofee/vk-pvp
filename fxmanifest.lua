9cf2f42a7fa7c20c8e43df8035672fab95d047f2
feelfreetofee <89666307+feelfreetofee@users.noreply.github.com>
2021-11-19 02:48:21 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+-- Resource Metadata
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Vikinga <vikin.ga>'
+description 'pvp'
+version '0.0.1'
+repository 'https://github.com/serferacu/vk-pvp'
+
+-- What to run
+client_scripts {
+    'client.lua',
+}
